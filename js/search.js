e5607b538f2ef2394f18f8dbc03362248d3084f8
scn2930 <scn2930@naver.com>
2024-06-25 17:56:57 +0900

    js -ing

--- js/search.js
+++ js/search.js
@@ -1,32 +1,4 @@
-let filterPeople = document.querySelectorAll('.people_count');
-let gameList = document.querySelectorAll('.games ul li');
-
-for (let fp of filterPeople) {
-  fp.addEventListener('click', (e) => {
-    for (let btn of filterPeople) {
-      btn.classList.remove('active');
-    }
-    e.target.classList.add('active');
-
-    let targetClass = e.target.getAttribute('data-filter');
-    console.log(targetClass);
-    let filteredList = document.querySelectorAll(`.games ul .${targetClass}`);
-    console.log(filteredList);
-
-    for (let gl of gameList) {
-      gl.style.display = 'none';
-    }
-     for (let fl of filteredList) {
-       fl.style.display = "block";
-    }
-
-  })
-
-}
-
-
 const logoImage = document.querySelector('h1 > a > img');
-
 function changeImageSource() {
     if (window.matchMedia("(max-width: 767px)").matches) {
         logoImage.src = './img/logo_only.png';
@@ -41,3 +13,56 @@ changeImageSource();
 
 // 브라우저 창 크기 변경 시 이미지 변경
 window.addEventListener('resize', changeImageSource);
+
+
+let filterPeople = document.querySelectorAll('.people_count');
+let gameList = document.querySelectorAll('.game-card');
+
+for (let fp of filterPeople) {
+  fp.addEventListener('click', (e) => {
+    for (let btn of filterPeople) {
+      btn.classList.remove('active');
+    }
+    e.target.classList.add('active');
+
+    let targetClass = e.target.getAttribute('data-filter');
+    console.log(targetClass);
+    let filteredList = document.querySelectorAll(`.game-row .${targetClass}`);
+    console.log(filteredList);
+
+    for (let gl of gameList) {
+      gl.style.display = 'none';
+    }
+     for (let fl of filteredList) {
+       fl.style.display = "block";
+       fl.classList.add('filter_layout');
+    }
+
+  })
+
+}
+
+let filterLevel = document.querySelectorAll(".level");
+// let levelList = document.querySelectorAll(".game-card");
+
+for (let fl of filterLevel) {
+  fl.addEventListener("click", (e) => {
+    for (let btn of filterLevel) {
+      btn.classList.remove("active");
+    }
+    e.target.classList.add("active");
+
+    let targetClass = e.target.getAttribute("data-filter");
+    console.log(targetClass);
+    let filteredList = document.querySelectorAll(`.game-row .${targetClass}`);
+    console.log(filteredList);
+
+    for (let gl of gameList) {
+      gl.style.display = "none";
+    }
+    for (let fl of filteredList) {
+      fl.style.display = "block";
+      fl.classList.add("filter_layout");
+    }
+  });
+}
